6eb96d82a0f38aa61450d12a99e0301effc7fb35
zhujn <247176894@qq.com>
2019-03-12 15:48:32 +0800

    01内容

---
+++ java01/src/com/demo/day01.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * Created by admin on 2019/3/12.
+ */
+public class day01 {
+    public static void main(String[] args){
+        System.out.println("hello" +
+                "world");
+    }
+}
